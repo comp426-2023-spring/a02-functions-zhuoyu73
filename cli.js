f15d2ad48338ca2b6baa0a7fc5d5ee93f1fcd14b
Zhuoyu Shi <zhuoyu73@email.unc.edu>
2023-03-01 00:56:11 -0500

    Fix error.

--- cli.js
+++ cli.js
@@ -8,17 +8,18 @@ const args = minimist(process.argv.slice(2));
 
 if (args.h){
     try{
-        console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
+        console.log(`
+            Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+                -h            Show this help message and exit.
+                -n, -s        Latitude: N positive; S negative.
+                -e, -w        Longitude: E positive; W negative.
+                -z            Time zone: uses tz.guess() from moment-timezone by default.
+                -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+                -j            Echo pretty JSON from open-meteo API and exit.
             `)
-        process.exit(0);
+        process.exitCode(0);
     }catch(err){
-        process.exit(1);
+        process.exitCode(1);
     }
 }
 
